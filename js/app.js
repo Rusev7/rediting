28f5d949a78ba5cef2d62eb8f61a7c67b9eeecd5
RRusev7 <rusi7890@abv.bg>
2020-08-25 16:21:38 +0300

    Added REditing portfolio

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Initializie image slider
+/*
+document.querySelectorAll('.cocoen').forEach(function(element){
+  new Cocoen(element);
+});
+*/// for more than one elements ^^^^^
+
+new Cocoen(document.querySelector('.cocoen'));
