e0af7aa2fa6c1340ee7e6e7f1a0abcecf915fb9a
RRusev7 <rusi7890@abv.bg>
2020-08-27 16:43:49 +0300

    added navi

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 // Initializie image slider
-/*
-document.querySelectorAll('.cocoen').forEach(function(element){
-  new Cocoen(element);
-});
-*/// for more than one elements ^^^^^
-
 new Cocoen(document.querySelector('.cocoen'));
+
+// Initialize scroll animations
 AOS.init();
+
+window.addEventListener('scroll', () => {
+    let windowHight = window.outerHeight;
+    let scrollLimit = Math.floor(windowHight / 400) * 100;
+    const navigation = document.querySelector('.navigation');
+
+    if(window.pageYOffset >= scrollLimit) {
+        navigation.classList.add('navi-appearance');
+        navigation.classList.remove('navi-disappear');
+    } else {
+        navigation.classList.remove('navi-appearance');
+        navigation.classList.add('navi-disappear');
+    }
+})
